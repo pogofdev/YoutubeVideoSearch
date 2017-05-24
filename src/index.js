35873306c1964a2a46537358bd2150aa54530b1a
poisongas <poisongass@yahoo.com>
2017-05-24 21:39:09 +0700

    inital commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCo2Rt3iO4x7syK7mfJXAv9PJ7uMAojmIc';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboard'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
